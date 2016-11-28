f274c0efaa9acf059f273fff3510057b38c5dce0
vladimir-trifonov <trifonov.dev@gmail.com>
2016-11-28 15:41:37 +0200

    fixes and changes

---
+++ src/app/theme/components/baLinksNav/baLinksNav.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ba-links-nav',
+  styles: [require('./baLinksNav.scss')],
+  template: require('./baLinksNav.html')
+})
+export class BaLinksNav {
+
+  constructor() {}
+
+}
